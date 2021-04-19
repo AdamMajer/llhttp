22b8f5bec8184e0a3e9b4f674cdb87d3bc3fac66
Fedor Indutny <fedor@indutny.com>
2021-04-18 17:07:23 -0700

    api: allow returning -1 in spans, doc HPE_USER
    `HPE_USER` return value was allowed in span callbacks (`on_body`, etc),
    but wasn't in the documentation. Additionally, looking at the return
    values of other documented callbacks it was easy to assume that `-1` was
    a valid return value for spans as well. Instead of returning it as it
    is change it to `HPE_USER` and add automatic error reason with the
    span's name.
    
    PR-URL: https://github.com/nodejs/llhttp/pull/104
    Reviewed-By: Daniele Belardi <dwon.dnl@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>

--- src/native/api.h
+++ src/native/api.h
@@ -15,6 +15,7 @@ struct llhttp_settings_s {
   /* Possible return values 0, -1, `HPE_PAUSED` */
   llhttp_cb      on_message_begin;
 
+  /* Possible return values 0, -1, HPE_USER */
   llhttp_data_cb on_url;
   llhttp_data_cb on_status;
   llhttp_data_cb on_header_field;
